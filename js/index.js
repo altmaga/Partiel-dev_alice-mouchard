b43754336e85f2608ab29e974d3c2cb1438b44f2
Alice <alice.mouchard85@gmail.com>
2018-12-19 09:05:49 +0100

    Trame partiel

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// Sélection des élements HTML "page"
+// à remplir
+
+// Scroll (click + scroll)
+// à remplir
+
+// Slider (clicks + effet de slide)
+// à remplir
+
+// Chargment des données et création des pages (fetch + appel de fonctions)
+// à remplir
+
+// page 1
+var createPageUn = function(data) {
+	// à remplir
+};
+
+// page 2
+var createPageDeux = function(data) {
+	// à remplir
+};
+
+// page 3
+var createPageTrois = function(data) {
+	// à remplir
+};
